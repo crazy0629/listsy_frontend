45d55c2771e7b5fa567e234ebdc26e0a3b890fb8
crazy0629 <kenzo.egghead@gmail.com>
2023-12-05 17:47:40 -0800

    add: Cleaning, Heating, Kitchen to Electronics

---
+++ src/modules/upload/detailsform/Electronics/Cleaning.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import * as Styled from "../details.styles";
+import { MultiSelection, SingleSelection } from "@/components";
+import { selectData } from "../data";
+import { toast } from "react-toastify";
+
+type Props = {
+  onSave: (data: any) => void;
+};
+
+export const Cleaning: React.FC<Props> = ({ onSave }) => {
+  const [form, setForm] = useState({
+    itemCondition: "",
+    type: "",
+    brand: "",
+    warrantyInformation: "",
+  });
+
+  const handleSave = () => {
+    if (!form.itemCondition) {
+      toast.error("Enter itemCondition");
+    } else {
+      onSave(form);
+    }
+  };
+
+  return (
+    <Styled.FormContainer>
+      <SingleSelection
+        data={selectData.forSale.Cleaning.Condition}
+        label="Item Condition"
+        placeholder="Select Item Condition"
+        value={form.itemCondition}
+        onChange={(value) =>
+          setForm((prev) => ({ ...prev, itemCondition: value }))
+        }
+      />
+      <SingleSelection
+        data={selectData.forSale.Cleaning.Type}
+        label="Type"
+        placeholder="Select Type"
+        value={form.type}
+        onChange={(value) => setForm((prev) => ({ ...prev, type: value }))}
+      />
+      <SingleSelection
+        data={selectData.forSale.Cleaning.Brands}
+        label="Brand"
+        placeholder="Select Brand"
+        value={form.brand}
+        onChange={(value) => setForm((prev) => ({ ...prev, brand: value }))}
+      />
+      <SingleSelection
+        data={selectData.forSale.Cleaning.WarrantyInformation}
+        label="Warranty Information"
+        placeholder="Select Warranty Information"
+        value={form.warrantyInformation}
+        onChange={(value) =>
+          setForm((prev) => ({ ...prev, warrantyInformation: value }))
+        }
+      />
+      <Styled.SaveButton onClick={handleSave}>Save</Styled.SaveButton>
+    </Styled.FormContainer>
+  );
+};
---
+++ src/modules/upload/detailsform/Electronics/TheaterSystems.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import * as Styled from "../details.styles";
+import { MultiSelection, SingleSelection } from "@/components";
+import { selectData } from "../data";
+import { toast } from "react-toastify";
+
+type Props = {
+  onSave: (data: any) => void;
+};
+
+export const TheaterSystems: React.FC<Props> = ({ onSave }) => {
+  const [form, setForm] = useState({
+    itemCondition: "",
+    type: "",
+    brand: "",
+    features: [] as string[],
+    warrantyInformation: "",
+  });
+
+  const handleSave = () => {
+    if (!form.itemCondition) {
+      toast.error("Enter itemCondition");
+    } else {
+      onSave(form);
+    }
+  };
+
+  return (
+    <Styled.FormContainer>
+      <SingleSelection
+        data={selectData.forSale.TheaterSystems.Condition}
+        label="Item Condition"
+        placeholder="Select Item Condition"
+        value={form.itemCondition}
+        onChange={(value) =>
+          setForm((prev) => ({ ...prev, itemCondition: value }))
+        }
+      />
+      <SingleSelection
+        data={selectData.forSale.TheaterSystems.Type}
+        label="Type"
+        placeholder="Select Type"
+        value={form.type}
+        onChange={(value) => setForm((prev) => ({ ...prev, type: value }))}
+      />
+      <SingleSelection
+        data={selectData.forSale.TheaterSystems.Brand}
+        label="Brand"
+        placeholder="Select Brand"
+        value={form.brand}
+        onChange={(value) => setForm((prev) => ({ ...prev, brand: value }))}
+      />
+      <MultiSelection
+        data={selectData.forSale.TheaterSystems.Features}
+        label="Features"
+        placeholder="Select Features"
+        value={form.features}
+        onChange={(value) => setForm((prev) => ({ ...prev, features: value }))}
+      />
+      <SingleSelection
+        data={selectData.forSale.TheaterSystems.WarrantyInformation}
+        label="Warranty Information"
+        placeholder="Select Warranty Information"
+        value={form.warrantyInformation}
+        onChange={(value) =>
+          setForm((prev) => ({ ...prev, warrantyInformation: value }))
+        }
+      />
+      <Styled.SaveButton onClick={handleSave}>Save</Styled.SaveButton>
+    </Styled.FormContainer>
+  );
+};
